d2739ba696ac8cc5880154a00637a6fb33d76df1
Kaelin Hoang <kaelin.hoang@gmail.com>
2020-10-30 14:47:38 -0600

    Add table sorting

--- js/table.js
+++ js/table.js
@@ -2,10 +2,15 @@ class Table {
     constructor(data, updateInfocard) {
         this.data = data;
         this.updateInfocard = updateInfocard;
+        this.colKeys = ["pokedex_number", "name", "type1", "type2", "hp", "attack", "defense", "sp_attack", "sp_defense", "speed"];
+        this.headerData = this.makeHeaderData();
+        
+        this.attachSortHandlers();
         this.drawTable();
     }
 
     drawTable() {
+        this.updateHeaders();
         let rows = d3.select("#table-body")
             .selectAll("tr")
             .data(this.data)
@@ -28,5 +33,80 @@ class Table {
     updateData(newData) {
         this.data = newData;
         this.drawTable();
+    }
+
+    updateHeaders() {
+        let colSel = d3.select("#headers");
+        colSel.selectAll("th")
+            .data(this.headerData)
+            .classed("sorting", d => d.sorted);
+
+        colSel.selectAll("i")
+            .data(this.headerData)
+            .classed('no-display', d => !d.sorted)
+            .classed('fa-sort-up', d => d.ascending && d.sorted)
+            .classed('fa-sort-down', d => !d.ascending && d.sorted);
+    }
+
+    attachSortHandlers() {
+        d3.select("#headers")
+            .selectAll("th")
+            .data(this.headerData)
+            .on("click", d => {
+                let sortAsc = d.sorted ? !d.ascending : true;
+                this.sortData(d.key, sortAsc, d.func);
+                this.headerData.forEach(h => h.sorted = false);
+                d.sorted = true;
+                d.ascending = sortAsc;
+                this.drawTable();
+            });
+    }
+
+    sortData(key, isAsc, func) {
+        this.data.sort((a, b) => {
+            let sortKey = key;
+            let x = a[sortKey];
+            let y = b[sortKey];
+
+            if (!isAsc) {
+                [x,y] = [y,x];
+            }
+            if (func) {
+                x = func(x);
+                y = func(y);
+            }
+            if (x < y) {
+                return -1;
+            }
+            else if (x > y) {
+                return 1;
+            }
+            return 0;
+        });
+    }
+
+    makeHeaderData() {
+        let headers = []
+        let strings = this.colKeys.slice(1, 4);
+        this.colKeys.forEach(k => {
+            if (strings.includes(k)) {
+                headers.push({
+                    sorted: false,
+                    ascending: false,
+                    key: k
+                });
+            }
+            else {
+                headers.push({
+                    sorted: false,
+                    ascending: false,
+                    key: k,
+                    func: d => +d
+                });
+            }
+        });
+
+
+        return headers;
     }
 }
