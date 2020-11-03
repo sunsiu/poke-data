7b32cd05fcf177383e8e33d47a8a727679903ea4
sunsiu <sunnytsiu@gmail.com>
2020-11-03 14:12:42 -0700

    added json converter and data

---
+++ csv_to_json.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import numpy as np
+import json
+
+
+def str_to_array(str):
+    str_array = json.loads(str.replace("'", '"'))
+    return [int(x) for x in str_array]
+
+
+evol_df = pd.read_csv("data/evolutions.csv")
+evol_df['long_id'] = evol_df.long_id.astype(np.int64)
+evol_df['ev_from'] = evol_df.ev_from.fillna(0)
+evol_df['ev_from'] = evol_df.ev_from.astype(np.int64)
+evol_df['ev_to'] = evol_df.ev_to.map(str_to_array)
+evol_df['evo_family'] =evol_df.evo_family.map(str_to_array)
+result = evol_df.to_json(orient="index")
+parsed = json.loads(result)
+
+with open("data/evolutions.json", 'w') as out:
+    json.dump(parsed, out)
+
+poke_df = pd.read_csv("data/pokemon.csv")
+result = poke_df.to_json(orient="index")
+parsed = json.loads(result)
+
+with open("data/pokemon.json", 'w') as out:
+    json.dump(parsed, out)
