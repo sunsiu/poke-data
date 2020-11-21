a1a6523ca32bb1877d846cdfd5272909cebc6b6d
sunsiu <sunnytsiu@gmail.com>
2020-11-20 22:50:38 -0700

    Made type chart

--- js/typeChart.js
+++ js/typeChart.js
@@ -1,9 +1,15 @@
 class TypeChart {
     constructor(typeData) {
         this.data = typeData;
-        this.types = ['water', 'normal', 'grass', 'bug', 'fire', 'psychic',
+        const types = ['water', 'normal', 'grass', 'bug', 'fire', 'psychic',
         'rock', 'electric', 'ground', 'dark', 'poison', 'fighting',
         'dragon', 'ghost', 'ice', 'steel', 'fairy', 'flying']
+
+        this.typeToIndex = new Map();
+        for(let i = 0; i < types.length; i++) {
+            this.typeToIndex.set(types[i], i);
+        }
+
         this.colors = ['#718bc6', '#a7a878', '#7cc251', '#a8b939', '#ef802e', '#f05888',
         '#b7a036', '#f8d031', '#e0c067', '#6c537a', '#d874d3', ' #c03228',
         '#6457a5', '#705999', '#98d7d6', '#b8b8cf', '#ee99ac', '#9f8fc4']
@@ -11,56 +17,152 @@ class TypeChart {
         // 0:neutral against, 1:weak against, 2:strong against, 3:no affect against
         this.legend = ['none', '#20639B80', '#F6D55C80', '#ED553B80'];
 
-        this.size = 750;
-        this.drawChords();
+        this.width = 1200;
+        this.height = 650;
+
+        this.smallPadding = 15;
+        this.bigPadding = 50;
+
+        this.drawArcChart();
     }
 
-    drawChords() {
-        const innerRadius = this.size * .39;
-        const outerRadius = innerRadius * 1.04;
-
-        let chord = d3.chord()
-            .padAngle(.04)
-            .sortSubgroups(d3.descending) /*sort the chords inside an arc from high to low*/
-            .sortChords(d3.descending) /*which chord should be shown on top when chords cross. Now the biggest chord is at the bottom*/
-
+    drawArcChart() {
         /*Initiate the SVG*/
         let svg = d3.select(".item-types").append("svg")
-            .attr("width", this.size)
-            .attr("height", this.size)
+        .attr("width", this.width)
+        .attr("height", this.height)
+        .append("g");
 
-        let chords = chord(this.data);
+        let relations = this.createRelations(this.data);
 
-        let g = svg.append("g")
-            .attr("transform", `translate(${this.size/2}, ${this.size/2})`)
+        let spacing = (this.width)/relations.nodes.length
+        let xPos = d3.scaleLinear()
+            .range([30, this.width - spacing])
+            .domain([0, relations.nodes.length-1])
 
-        let arc = d3.arc()
-            .innerRadius(innerRadius)
-            .outerRadius(outerRadius);
-            
-        let groups = g.append("g")
-            .classed("groups", true)
-            .selectAll("g")
-            .data(chords.groups)
-            .join("g")
-            
-        groups.append("path")
-              .attr("class", "arc")
-              .style("stroke", d => this.colors[d.index])
-              .style("fill", d => this.colors[d.index])
-              .attr("d", arc);
-
-        let ribbon = d3.ribbon()
-            .radius(innerRadius);
-
-        console.log(chords)
-        let ribbons = g.append("g")
-            .attr("class", "ribbons")
+        svg.append("g")
+            .classed("line-group", true)
             .selectAll("path")
-            .data(chords)
+            .data(relations.links)
             .join("path")
-            .attr("d", ribbon)
-            .style("stroke", d => this.legend[d.source.value])
-            .style("fill", d => this.legend[d.source.value])
+            .classed("type-line", true)
+            .attr("d", d => {
+                const start = xPos(this.typeToIndex.get(d.from));
+                const end = xPos(this.typeToIndex.get(d.to));
+                return ['M', start, this.height-this.bigPadding,    // the arc starts at the coordinate x=start, y=height-30 (where the starting node is)
+                'A',                            // This means we're gonna build an elliptical arc
+                (start - end)/2, ',',    // Next 2 lines are the coordinates of the inflexion point. Height of this point is proportional with start - end distance
+                (start - end)/2, 0, 0, ',',
+                start < end ? 1 : 0, end, ',', this.height-this.bigPadding] // We always want the arc on top. So if end is before start, putting 0 here turn the arc upside down.
+                .join(' ');
+            })
+
+        let nodes = svg.append("g")
+            .classed("node-group", true)
+            .selectAll("circle")
+            .data(relations.nodes)
+            .join("circle")
+            .classed("type-circle", true)
+            .attr("cx", (_, i) => xPos(i))
+            .attr("cy", this.height - this.bigPadding)
+            .attr("r", 15)
+            .style("fill", (_, i) => this.colors[i])
+
+        svg.append("g")
+            .classed("label-group", true)
+            .selectAll("text")
+            .data(relations.nodes)
+            .join("text")
+            .classed("type-label", true)
+            .attr("x", (_, i) => xPos(i))
+            .attr("y", this.height - this.smallPadding)
+            .text(d => d)
+
+        nodes.on('mouseover', function(d) {
+            d3.select(this).style("stroke-width", 2);
+
+            let nodeLinks = d3.selectAll(".type-line");
+            nodeLinks.classed("blurred-line", true);
+
+            // Strong or no effect against
+            nodeLinks.filter(link => link.from === d)
+                .attr("class", link => {
+                    const prepend = "type-line ";
+                    if(link.relation == "strong_against")
+                        return prepend + "highlight-strong";
+                    else if(link.relation == "weak_against")
+                        return prepend + "highlight-weak";
+                    else if(link.relation == "no_effect_against")
+                        return prepend + "highlight-no-effect";
+
+                    return prepend;
+                })
+                .raise()
+        })
+        .on("mouseout", function(d) {
+            d3.select(this).style("stroke-width", 1)
+            d3.selectAll(".type-line").attr("class", "type-line");
+        })
+
+        this.drawLegend();
+    }
+
+    createRelations(data) {
+        let result = {};
+        result.links = [];
+        data.forEach(d => {
+            const name = d.type;
+            if (d.strong_against.length > 0)
+                d.strong_against.forEach(t => {
+                    result.links.push({from: name, to: t, relation: "strong_against"})
+                })
+                
+            if (d.weak_against.length > 0)
+                d.weak_against.forEach(t => {
+                    result.links.push({from: name, to: t, relation: "weak_against"})
+                })
+            if (d.no_effect_against.length > 0)
+                d.no_effect_against.forEach(t => {
+                    result.links.push({from: name, to: t, relation: "no_effect_against"})
+                })
+        })
+
+        result.nodes = data.map(d => d.type);
+        return result
+    }
+
+    drawLegend() {
+        let legendGroup = d3.select(".item-types")
+            .select("svg")
+            .append("g")
+            .classed("legend-group", true)
+            .attr("transform", "translate(15, 15)")
+
+        let item1 = legendGroup.append("g");
+        item1.append("line")
+            .attr("x1", 0)
+            .attr("x2", 25)
+            .classed("highlight-strong", true)
+        item1.append("text")
+            .text("Strong Against")
+            .attr("transform", "translate(30, 5)")
+
+        let item2 = legendGroup.append("g").attr("transform", "translate(0, 25)");
+        item2.append("line")
+            .attr("x1", 0)
+            .attr("x2", 25)
+            .classed("highlight-weak", true)
+        item2.append("text")
+            .text("Weak Against")
+            .attr("transform", "translate(30, 5)")
+
+        let item3 = legendGroup.append("g").attr("transform", "translate(0, 50)");
+        item3.append("line")
+            .attr("x1", 0)
+            .attr("x2", 25)
+            .classed("highlight-no-effect", true)
+        item3.append("text")
+            .text("No Effect Against")
+            .attr("transform", "translate(30, 5)")
     }
 }
--- types_collector.py
+++ types_collector.py
@@ -1,24 +1,34 @@
 import requests
 import json
-import numpy as np
 
 # API src https://pokeapi.co/docs/v2#types
 types = ["water", "normal", "grass", "bug", "fire", "psychic", "rock", "electric", "ground", "dark", "poison", "fighting", "dragon", "ghost", "ice", "steel", "fairy", "flying"] 
-result = np.zeros((18, 18), dtype=int)
+result = []
 for i, t in enumerate(types):
     res = requests.get("https://pokeapi.co/api/v2/type/{}".format(t))
     data = res.json()
     relations = data["damage_relations"]
-    row = result[i]
-    for x in [d['name'] for d in relations["double_damage_from"]]:
-        row[types.index(x)] = 1
 
+    strong_against = []
     for x in [d['name'] for d in relations["double_damage_to"]]:
-        row[types.index(x)] = 2
+        strong_against.append(x)
 
+    weak_against = []
+    for x in [d['name'] for d in relations["half_damage_to"]]:
+        weak_against.append(x)
+
+    no_effect_against = []
     for x in [d['name'] for d in relations["no_damage_to"]]:
-        row[types.index(x)] = 3
+        no_effect_against.append(x)
+
+    payload = {
+        "type": t,
+        "strong_against": strong_against,
+        "weak_against": weak_against,
+        "no_effect_against": no_effect_against
+    }
+    result.append(payload)
 
 f = open('data/types.json', "w")
-json.dump(result.tolist(), f)
+json.dump(result, f)
 f.close()
